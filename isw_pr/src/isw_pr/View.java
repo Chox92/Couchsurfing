17fd70985aa81dd4e8175211964ff7730c72d228
Chox92 <mr.chip92@yahoo.it>
2016-02-10 11:43:09 +0100

    View.java

---
+++ isw_pr/src/isw_pr/View.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isw_pr;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class View {
+    
+    public View(){
+        
+        System.out.println("Buongiorno!\n");
+        System.out.println("Vuoi effettuare la registrazione o il progetto?");
+    }
+}
